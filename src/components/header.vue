69f40c52c89e1bf0546498bfa88fedd0085bd4d8
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-21 13:09:01 +0200

    created nav-bar on header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark px-5 py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand text-danger" href="#">BOOLFIX</a>
+                <button class="navbar-toggler">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchImput" >
+                        <button class="btn btn-outline-danger" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script>
 export default {
+    data : function(){
+        return{
+            searchImput : '',
+        }
+    }
 
 }
 </script>

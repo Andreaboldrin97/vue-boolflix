e4e80a79d66cd8df9f5d1ba29c6d64034e371f4c
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-21 15:52:54 +0200

    create props on filmCard.vue

---
+++ src/components/filmCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul>
+            <li>{{ filmTitle }}</li>
+            <li> {{ originalTitleFilm }}</li>
+            <li>{{ originalLanguageFilm }}</li>
+            <li>{{ filmVote }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    props : {
+        filmTitle : String,
+        originalTitleFilm : String,
+        originalLanguageFilm : String,
+        filmVote : [String,Number]
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>

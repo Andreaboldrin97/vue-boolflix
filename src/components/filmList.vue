cea55d0c0a3da75ecee93362a266e270112e727f
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-21 12:42:46 +0200

    created components for the project

---
+++ src/components/filmList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <cardFilm />
+    </div>
+</template>
+
+<script>
+import cardFilm from './components/cardFilm.vue' 
+
+export default {
+    components : {
+        cardFilm
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <filmList />
+    </div>
+</template>
+
+<script>
+import filmList from './components/filmList.vue' 
+
+export default {
+    components :{
+        filmList
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>s

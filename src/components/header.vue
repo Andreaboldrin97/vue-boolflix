125b3789a30314bbf0d4160b6f09a2da8a85ddd8
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-25 16:35:27 +0200

    add google font roboto

--- src/components/header.vue
+++ src/components/header.vue
@@ -6,8 +6,8 @@
                     <span class="navbar-toggler-icon btn-nav"></span>
                 </button>
             </div>
-            <div class="d-flex justify-content-between w-100 align-items-center">
-                    <a class="navbar-brand text-danger" href="#">BOOLFIX</a>
+            <div class="d-flex justify-content-between w-100 align-items-center logo-text">
+                    <a class="navbar-brand text-danger " href="#">BOOLFIX</a>
                     <div>
                         <input class="form-control me-2" placeholder="Search" v-model.trim="searchInput" @keyup.enter="$emit('search' , searchInput)">
                     </div>
@@ -37,5 +37,12 @@ export default {
 <style lang="scss">
 header{
     height: 88px;
+}
+.logo-text{
+    a{
+        font-size: 35px;
+        font-weight: bold;
+    }
+    
 }
 </style>
